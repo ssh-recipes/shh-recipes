f8a87a90fd20d281d1dab18886c5e37079ed0229
Freddy Snow <fjsn@protonmail.com>
2024-11-23 15:38:30 +0000

    env change, added hono's nod e adapter

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+const { config } = require('dotenv');
+config();
+
+const { serve } = require('@hono/node-server');
+const { Hono } = require('hono');
+
+const app = new Hono();
+app.get('/', (c) => c.text('Hello world'));
+
+serve({
+  fetch: app.fetch,
+  port: process.env.PORT,
+})
