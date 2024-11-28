93efed92657170ffed44a26b41ed9751fb42399b
mariodamas <mariodamassanchez@gmail.com>
2024-11-28 12:54:35 +0100

    getUser endpoint modified to get all the info

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,20 +3,25 @@ import db from "../db.js";
 
 const app = new Hono();
 
-app.get('/:id', async (c) => {
+app.get('/', async (c) => {
     try {
-        const id = c.req.param('id');
-        const [rows] = await db.query('SELECT * FROM User WHERE id = ?', [id]);
-        
-        if (rows.length === 0) {
-            return c.json({ success: false, error: 'User not found' }, 404);
-        }
+        const [rows] = await db.query('SELECT * FROM User WHERE id = 1');
+        const user = rows[0];
 
-        return c.json({ success: true, user: rows[0] });
+        const [rules] = await db.query('SELECT r.name FROM Rule r, UserRule ur WHERE ur.rule_id = r.id AND ur.user_id = ?', [user.id]);
+
+        return c.json({
+            success: true,
+            data: {
+                name: user.name,
+                rules: rules.map(rule => rule.name),
+            },
+        });
     } catch (err) {
         console.error(err);
         return c.json({ success: false, error: 'Error with the user' }, 500);
     }
 });
+
 
 export default app;
